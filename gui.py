464db389dc8257176740bdc8a0de9716c6c79178
JohanAndres19 <johanandresa@hotmail.com>
2021-04-15 00:13:39 -0500

    arreglando la interfaz

--- gui.py
+++ gui.py
@@ -1,16 +1,39 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene
+from PyQt5.QtGui import QFont, QPen,QPainter,QBrush
+from PyQt5.QtCore import Qt
 
 class Ventana_principal(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ventana_principal.ui",self)
-        #self.Boton_agregar.clicked.connect()
+        self.escena= QGraphicsScene()
+        self.green_brush =QBrush(Qt.green)
+        self.blue_brush=QBrush(Qt.blue)
+        self.canvas.backgroundBrush(self.green_brush)
+        self.pen =QPen(Qt.red)
+        self.texto.setClearButtonEnabled(True)
+        self.texto_traducido.setClearButtonEnabled(True)
+        self.Boton_agregar.clicked.connect(self.Agregar)
 
     def Agregar(self):
-        self.texto
+        a=self.texto.text()
+        b=self.texto_traducido.text()
+        self.texto.clear()
+        self.texto_traducido.clear()
+        print(a,b)
 
+    def paintEvent(self, e):
+        painter = QPainter(self.canvas)
+        painter.setPen(self.pen)
+        painter.drawLine(0,0,10,0)   
+
+
+class modelo:
+
+    def __init__(self) :
+        pass
 
 
 
