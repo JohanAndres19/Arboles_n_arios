cd95ca084ad3b945f80e92736ff6053822a0f0af
JohanAndres19 <johanandresa@hotmail.com>
2021-04-17 10:38:27 -0500

    interfaz casi lista falta logica

--- gui.py
+++ gui.py
@@ -1,21 +1,27 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene
-from PyQt5.QtGui import QFont, QPen,QPainter,QBrush
+from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene, QWidget
+from PyQt5.QtGui import QFont, QPalette, QPen,QPainter,QBrush
 from PyQt5.QtCore import Qt
 
 class Ventana_principal(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ventana_principal.ui",self)
-        self.escena= QGraphicsScene()
-        self.green_brush =QBrush(Qt.green)
-        self.blue_brush=QBrush(Qt.blue)
-        self.canvas.backgroundBrush(self.green_brush)
-        self.pen =QPen(Qt.red)
+        self.setAutoFillBackground(True)
+        Paleta_color =self.palette()
+        Paleta_color.setColor(self.backgroundRole(),Qt.cyan)
+        self.setPalette(Paleta_color)
+        self.canvas = canvas(self.contenedor_canvas)
+        self.canvas.resize(self.contenedor_canvas.width(),self.contenedor_canvas.height())
+        self.canvas.setAutoFillBackground(True)
+        self.canvas.setBackgroundRole(QPalette.Base)
         self.texto.setClearButtonEnabled(True)
+        self.texto_eliminar.setClearButtonEnabled(True)
+        self.texto_buscar.setClearButtonEnabled(True)
         self.texto_traducido.setClearButtonEnabled(True)
-        self.Boton_agregar.clicked.connect(self.Agregar)
+        self.Boton_agregar.clicked.connect( lambda:self.canvas.dibujar(self.texto,self.texto_traducido))
+        
 
     def Agregar(self):
         a=self.texto.text()
@@ -23,11 +29,49 @@ class Ventana_principal(QMainWindow):
         self.texto.clear()
         self.texto_traducido.clear()
         print(a,b)
+       
+    
+class canvas (QWidget):
 
-    def paintEvent(self, e):
-        painter = QPainter(self.canvas)
-        painter.setPen(self.pen)
-        painter.drawLine(0,0,10,0)   
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+        self.a=None
+        self.b =None
+
+
+    def dibujar(self,valor1,valor2):
+        self.a=valor1.text()
+        self.b=valor2.text()
+        valor1.clear()
+        valor2.clear()
+        self.update()
+
+
+    def paintEvent(self,evento) :
+        painter = QPainter()
+        painter.begin(self)
+        if self.a != None and self.b!=None:
+            self.pen =QPen(Qt.red)
+            painter.setPen(self.pen)
+            """
+            painter.drawText(30,30,16,15,Qt.AlignCenter,self.a[0])
+            painter.drawLine(36,46,36,61)
+            painter.drawText(30,62,16,15,Qt.AlignCenter,self.a[1])
+            painter.drawLine(36,78,36,93)
+            painter.drawText(30,94,16,15,Qt.AlignCenter,self.a[2])
+            painter.drawLine(36,110,36,125)
+            painter.drawText(30,126,16,15,Qt.AlignCenter,self.a[3])
+            """
+            for i in range(len(self.a)):
+                if i == 0:
+                    painter.drawText(30,30,16,15,Qt.AlignCenter,self.a[i])
+                    painter.drawLine(36,46,36,61)
+                elif i== (len(self.a)-1):
+                    painter.drawText(30,30+(32*i),16,15,Qt.AlignCenter,self.a[i])
+                else:    
+                    painter.drawText(30,30+(32*i),16,15,Qt.AlignCenter,self.a[i])
+                    painter.drawLine(36,46+(32*i),36,61+(32*i))
+
 
 
 class modelo:
